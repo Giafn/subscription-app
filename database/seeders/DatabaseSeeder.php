74e63a2ccbbea77a64b2ec1a3aa89d2426b51cbc
giafn <giafauzan11@gmail.com>
2025-07-16 18:12:47 +0700

    update seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Super Admin',
             'email' => 'admin@admin.com',
             'password' => bcrypt('password'),
+            'email_verified_at' => now(),
             'role' => 'admin',
         ]);
 
